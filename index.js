ef9b3482c78e936dce03139ddb0c85e3976f300a
alfredwang0707 <alfredwang0707@gmail.com>
2020-12-09 15:29:30 -0500

    commit tree 9865960084e23a9a01247b3cf359af4a29dfd7b8

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendDriver(name){
+    drivers.push(name);
+}
+
+// 1) prepends a driver to the beginning of the drivers array
+function destructivelyPrependDriver(name){
+    drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver () {
+    drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver () {
+    drivers.shift();
+}
+
+// spread the drivers + the params
+function appendDriver (name) {
+    return [...drivers, name];
+  }
+
+function prependDriver(name){
+    return [name, ...drivers];
+}
+
+function removeLastDriver(){
+    return drivers.slice(0, -1);
+}
+
+function removeFirstDriver(){
+    return drivers.slice(1);
+}
